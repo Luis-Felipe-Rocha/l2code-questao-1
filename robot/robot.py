5778dc1c9c8805d01fd34f8ded991d4f9dd30a69
Luis Felipe Rocha Caixeta <luisfeliperc12@gmail.com>
2022-11-16 16:25:54 -0300

    fix: resolve negative positions

--- robot/robot.py
+++ robot/robot.py
@@ -16,23 +16,23 @@ class Robot:
     def _move(self, command):
 
         if command == 'F':
-            if self._direction == 'N':
+            if self._direction == 'N' and self._y_axis != self._width:
                 self._y_axis += 1
-            elif self._direction == 'O':
+            elif self._direction == 'O' and self._x_axis != 0:
                 self._x_axis -= 1
-            elif self._direction == 'S':
+            elif self._direction == 'S' and self._x_axis != 0:
                 self._y_axis -= 1
-            elif self._direction == 'L':
+            elif self._direction == 'L' and self._y_axis != self._lenght:
                 self._x_axis += 1
 
         elif command == 'T':
-            if self._direction == 'N':
+            if self._direction == 'N' and self._y_axis != 0:
                 self._y_axis -= 1
-            elif self._direction == 'O':
+            elif self._direction == 'O' and self._x_axis != self._lenght:
                 self._x_axis += 1
-            elif self._direction == 'S':
+            elif self._direction == 'S' and self._y_axis != self._width:
                 self._y_axis += 1
-            elif self._direction == 'L':
+            elif self._direction == 'L' and self._x_axis != 0:
                 self._x_axis -= 1
 
     def _turn(self, command):
